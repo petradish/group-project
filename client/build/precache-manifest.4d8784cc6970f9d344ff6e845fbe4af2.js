b6acacd5e503dbd347d6a1e208bf88059de39b59
Petra Laohakul <petra@xh.io>
2021-02-05 00:04:30 -0500

    Change redirect

---
+++ client/build/precache-manifest.4d8784cc6970f9d344ff6e845fbe4af2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1647f1a3c4afe6e2c95ee7b0081c7599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea245c8e0f8a723557a4",
+    "url": "/static/css/main.6f60f8ad.chunk.css"
+  },
+  {
+    "revision": "d72803a9b11e9877c64e",
+    "url": "/static/js/2.177d0751.chunk.js"
+  },
+  {
+    "revision": "1ad9108481b0ef10fc8c5fb3f1ade286",
+    "url": "/static/js/2.177d0751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea245c8e0f8a723557a4",
+    "url": "/static/js/main.80d615b6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
